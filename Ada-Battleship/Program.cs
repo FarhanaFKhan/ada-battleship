8cc41a358e8ae5967987f66cb7ceec80531f95b1
Farhana Khan <farhana.khan@ao.com>
2021-10-12 16:26:00 +0100

    setup project locally

---
+++ Ada-Battleship/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ada_Battleship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
