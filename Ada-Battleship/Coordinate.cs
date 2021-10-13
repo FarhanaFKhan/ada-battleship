2a6da1fe878f67b34d1f0749cb3260fa864f7467
Farhana Khan <farhana.khan@ao.com>
2021-10-13 23:37:13 +0100

    create classes

---
+++ Ada-Battleship/Coordinate.cs
@@ -0,0 +1,14 @@
+﻿namespace Ada_Battleship
+{
+    public class Coordinate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Ada-Battleship/Tile.cs
@@ -0,0 +1,12 @@
+﻿namespace Ada_Battleship
+{
+    public class Tile
+    {
+        public Coordinate Coordinate { get; set; }
+
+        public Tile(int x, int y)
+        {
+            Coordinate = new Coordinate(x, y);
+        }
+    }
+}
